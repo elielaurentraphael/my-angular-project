9398bf0f187f9ba22e8c39d03eb6c7a8930b4459
elielaurentraphael <raphelaur@gmail.com>
2024-02-14 22:16:34 +0300

    commit n°4: l'interpolation directe - du fichier appareil.component.ts: appareilName = 'Machine à laver' - vers le fichier appareil.component.html: ...{{ appareilName }}...

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrl: './appareil.component.scss'
+  styleUrl: './appareil.component.scss',
 })
 export class AppareilComponent {
-
+  appareilName = 'Machine à laver';
+  appareilStatus = 'éteint';
 }
