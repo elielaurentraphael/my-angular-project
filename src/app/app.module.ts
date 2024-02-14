94ace2530574df50dbfe670a6f0949572536949e
elielaurentraphael <raphelaur@gmail.com>
2024-02-14 21:10:52 +0300

    commit n°2: création d'un component - dans le terminal: ng generate component appareil - ça nous crée 1 component appelé AppareilComponent, formé par 3 fichiers (appareil.component.ts, appareil.component.html, appareil.component.scss); + mis à jour du fichier app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppareilComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrl: './appareil.component.scss'
+})
+export class AppareilComponent {
+
+}
