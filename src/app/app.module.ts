11b34c80ba35b4f64963733b993df0c9fccf3ee1
elielaurentraphael <raphelaur@gmail.com>
2024-02-15 08:41:24 +0300

    commit n°8: TWO WAY BINDING - Liaison en double sens - Du code typescript => DOM et du DOM => code typescript - Pour pouvoir utiliser TWO WAY BINDING - 1 - il faut importer FormsModule (depuis @angular forms) dans AppModule et - 2 - Dans appareil.component.html, créons une input de type texte (par exemple) avec ... [(ngModel)] = 'appareilName'... Comme ça, le texte qu'on écrit dans input va remplacer appareilName et, la valeur (essayer de changer cette valeur et voir input) de appareilName dans la class AppareilComponent se dessine forcement dans input.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, AppareilComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
