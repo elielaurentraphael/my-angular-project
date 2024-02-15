e142da7f8fcd9e76ffd7b51b9b495a682fca5960
elielaurentraphael <raphelaur@gmail.com>
2024-02-15 06:00:28 +0300

    commit n°6: PROPERTY BINDING - Modification des propriétés d'un élément du DOM en fonction des données dans le typescript (.ts) ou venant d'un serveur - 1 - Dans app.component.html, créons un bouton invalide qui devient valide si l'user est authentifié - 2 - Dans app.component.ts, créons une variable booléenne isAuth qui est false par défaut (on va simuler la procédure d'authentification avec la fonction setTimeout), qui dit si l'user est authentifié ou non - 3 - liaison par property binding ...[disabled] = '!isAuth'... l'état (disabled or enabled) du bouton dépendra de l'isAuth (true or false) de l'user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  // par défaut, l'user n'est pas authentifié
+  isAuth = false;
+
+  constructor() {
+    // simulation de la prcédure d'authentification de l'user
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+}
