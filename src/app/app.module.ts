8c220d4e068b803b0fe74608309ad9d42f01b5ae
elielaurentraphael <raphelaur@gmail.com>
2024-02-28 05:45:08 +0300

    commit n°20: insertion du service AppareilService dans l'application my-angular-project toute entière - 1 - dans app.module.ts, importer AppareilService depuis son dossier et, - 2 - créez son unique instance dans l'array providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
 
 @NgModule({
   declarations: [AppComponent, AppareilComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [AppareilService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
