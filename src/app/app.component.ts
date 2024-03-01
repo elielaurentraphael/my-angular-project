13f291bd01d49867b663563e7b74643d2eb000bc
elielaurentraphael <raphelaur@gmail.com>
2024-03-01 06:11:41 +0300

    commit n°30: restructuration de l'application - 1 - génération de deux nouveaux component (AuthComponent et AppareilViewComponent) - 2 - mise à jour automatique de app.module.ts - dans app.component.ts: - 3 - déménagement du contenu de la class AppComponent vers la class AppareilViewComponent - 4 - suppression de l'interface OnInit et du service AppareilService - dans appareil-view.component.ts: - 5 - importation de l'interface OnInit depuis angularcore - 6 - implémentation de l'interface OnInit dans la class AppareilViewComponent - 7 - importation du AppareilService depuis son dossier - 8 - dans app.component.html, déplacez la <div class='col-xs-12'>...</div> vers appareil-view.component.html et mettre la balise <app-appareil-view></app-appareil-view> à sa place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  // par défaut, l'user n'est pas authentifié
-  isAuth = false;
-
-  // simulation d'aller chercher la date au backend (sur un serveur)
-  lastUpdate: Promise<Date> = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(() => {
-      resolve(date);
-    }, 4000);
-  });
-
-  tableauDesAppareils!: any[];
-
-  // injection de AppareilService dans AppComponent
-  constructor(private appareilService: AppareilService) {
-    // simulation de la prcédure d'authentification de l'user
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 4000);
-  }
-
-  // angular cherche tableauDesAppareils dans AppareilService
-  ngOnInit() {
-    this.tableauDesAppareils = this.appareilService.appareils;
-  }
-
-  // AppComponent appelle la méthode switchOnAll() de AppareilService par sa méthode onOnAll()
-  onOnAll() {
-    this.appareilService.switchOnAll();
-  }
-
-  // AppComponent appelle la méthode switchOffAll() de AppareilService par sa méthode onOffAll()
-  onOffAll() {
-    this.appareilService.switchOffAll();
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
 @NgModule({
-  declarations: [AppComponent, AppareilComponent],
+  declarations: [AppComponent, AppareilComponent, AuthComponent, AppareilViewComponent],
   imports: [BrowserModule, FormsModule],
   providers: [AppareilService],
   bootstrap: [AppComponent],
