90c35dedc66a4aba3d2d14e7195511059e894d65
elielaurentraphael <raphelaur@gmail.com>
2024-03-01 18:13:16 +0300

    commit n°34: mis en œuvre du AuthService - dans auth.component.ts: - 1 - importation de l'interface OnInit depuis angular/core - 2 - importation du AuthService depuis son dossier - 3 - implémentation de l'interface OnInit dans AuthComponent - 4 - création d'une propriété locale authStatus de type boolean - 5 - injection de AuthService dans AuthComponent - 6 - initialisation de authStatus avec la méthode ngOnInit - 7 - création d'une méthode d'appel de la méthode signIn (signIn est une méthode asynchrone) du service AuthService - 8 - création d'une méthode d'appel de la méthode signOut du service AuthService - dans auth.component.html: - 9 - construction d'une page d'authentification très simple avec deux button (connexion et déconnection)

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+// importation de l'interface OnInit depuis angular/core
+import { Component, OnInit } from '@angular/core';
+// importation du AuthService depuis son dossier
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
+// implémentation de l'interface OnInit dans AuthComponent
+export class AuthComponent implements OnInit {
+  // création d'une propriété locale authStatus de type boolean
+  authStatus!: boolean;
 
+  // injection de AuthService dans AuthComponent
+  constructor(private authService: AuthService) {}
+
+  // authStatus sera toujours égale à isAuth (isAuth = false -> authStatus = false; isAuth = true -> authStatus = true)
+  ngOnInit() {
+    this.authStatus = this.authService.isAuth;
+  }
+
+  // méthode d'appel de la méthode signIn()
+  onSignIn() {
+    this.authService.signIn().then(() => {
+      alert('connexion reussie !');
+      // mise à jour de la propriété locale authStatus
+      this.authStatus = this.authService.isAuth;
+    });
+  }
+
+  // méthode d'appel de la méthode signOut()
+  onSignOut() {
+    this.authService.signOut();
+    this.authStatus = this.authService.isAuth;
+  }
 }
