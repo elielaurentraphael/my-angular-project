86c6da18971c98aded168e5b7f493d62f2aee8de
elielaurentraphael <raphelaur@gmail.com>
2024-03-01 07:25:33 +0300

    commit n°31: implatation du routing dans l'application - dans app.module.ts - 1 - importer Routes depuis angular/router - 2 - créons une constante appRoutes de type Routes qui va contenir les routes - 3 - importer RouterModule depuis angular/router - 4 - inserez RouterModule.forRoot(appRoutes) dans l'array imports - 5 - dans dans app.component.html, remplaçons <app-appareil-view></app-appareil-view> par <router-outlet></router-outlet>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, AppareilComponent, AuthComponent, AppareilViewComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [AppareilService],
   bootstrap: [AppComponent],
 })
