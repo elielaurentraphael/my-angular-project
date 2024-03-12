8dfb9525a82776135ff04dc32892c698ea768386
elielaurentraphael <raphelaur@gmail.com>
2024-03-12 17:25:20 +0300

    commit n°43: L'objet de type Subscription, counterSubscription, peut être détruit au moment venu - 1 - envelopper l'exécution de la méthode subscribe dans l'objet counterSubscription (importation de Subscription, ajout d'une propriété counterSubscription et, modification de la méthode ngOnInit) - 2 - destruction de l'objet counterSubscription (pour éviter les bugs du comportement infini) avec la methode ngOnDestroy de angular/core (importation de l'interface OnDestroy depuis angular/core, implémentation de cette interface dans la class AppComponent et, création de la méthode ngOnDestroy)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,29 @@
-// importation de l'interface OnInit
-import { Component, OnInit } from '@angular/core';
-// importation de l'Observable interval
-import { interval } from 'rxjs';
+// importation de l'interface OnDestroy
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// importation de Subscription depuis rxjs
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  // propriété qui stocke la durée de connexion de l'user
+export class AppComponent implements OnInit, OnDestroy {
+  // ajout d'une propriété de type Subscription
   secondes!: number;
+  counterSubscription!: Subscription;
 
   ngOnInit() {
     // création de l'Observable
     const counter = interval(1000);
-    // observation de l'Observable
-    counter.subscribe(
-      // affichage de la valeur émise par l'Observable
-      (value: number) => {
-        this.secondes = value;
-      },
-      // dans le cas d'une erreur eventuelle, le message d'erreur remplace l'affichage de la valeur
-      (error: any) => {
-        console.log('Une erreur a été renconterée !');
-      },
-      // dans le cas d'une complétion, le message de complétion remplace l'affichage de la valeur
-      () => {
-        console.log('Observable complétée !');
-      }
-    );
+    // affichage de la valeur émise par l'Observable. Stockage de l'exécution de la méthode subscribe() dans l'objet (destructible) counterSubscription
+    this.counterSubscription = counter.subscribe((value: number) => {
+      this.secondes = value;
+    });
+  }
+
+  ngOnDestroy() {
+    // destruction de l'objet counterSubscription (pour éviter les bugs du comportement infini du compteur)
+    this.counterSubscription.unsubscribe();
   }
 }
