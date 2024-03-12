e7f55e618c8060caabb2dee3c9771790b9ca9dc9
elielaurentraphael <raphelaur@gmail.com>
2024-03-12 12:33:47 +0300

    commit n°42: Observable avec comportement infini - dans app.component.ts - 1 - importer l'interface OnInit depuis angular/core puis, l'implémenter dans la class AppComponent et, créer la méthode ngOnInit - 2 - importer interval depuis rxjs - 3 - ajouter une propriété secondes de type number qui compte la durée de connexion de l'user - 4 -  dans ngOnInit, création du counter (qui va compter infiniment) puis, observation de l'Observabe counter (affichage de la valeur émise par seconde ou message d'une erreur éventuelle ou, message de complétion) - 5 - dans app.component.html: affichage du compteur de connexion de l'user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+// importation de l'interface OnInit
+import { Component, OnInit } from '@angular/core';
+// importation de l'Observable interval
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  // propriété qui stocke la durée de connexion de l'user
+  secondes!: number;
+
+  ngOnInit() {
+    // création de l'Observable
+    const counter = interval(1000);
+    // observation de l'Observable
+    counter.subscribe(
+      // affichage de la valeur émise par l'Observable
+      (value: number) => {
+        this.secondes = value;
+      },
+      // dans le cas d'une erreur eventuelle, le message d'erreur remplace l'affichage de la valeur
+      (error: any) => {
+        console.log('Une erreur a été renconterée !');
+      },
+      // dans le cas d'une complétion, le message de complétion remplace l'affichage de la valeur
+      () => {
+        console.log('Observable complétée !');
+      }
+    );
+  }
+}
